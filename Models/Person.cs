d88f9989c1e95c71da0a5c904bf23082c5c937ec
Matheus Nascimento - Ubuntu <matheusrnascimento92@gmail.om>
2025-01-13 22:52:57 -0300

    refactor: Ajustes na entidade de pessoas

--- Models/Person.cs
+++ Models/Person.cs
@@ -7,18 +7,18 @@ namespace RinhaDeBackEnd2023.Models
 {
     public class Person : BaseEntity
     {
-        public string Nickname { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string Birthdate { get; set; } = string.Empty;
-        public string[] Stack { get; set; }
+        public string apelido { get; set; } = string.Empty;
+        public string nome { get; set; } = string.Empty;
+        public string nascimento { get; set; } = string.Empty;
+        public string[] stack { get; set; }
 
         public Person(string nickname, string name, string birthdate, string[] stack = null)
         {
             Id = Guid.NewGuid();
-            this.Nickname = nickname;
-            this.Name = name;
-            this.Birthdate = birthdate;
-            this.Stack = stack;
+            this.apelido = nickname;
+            this.nome = name;
+            this.nascimento = birthdate;
+            this.stack = stack;
         }
     }
 }
